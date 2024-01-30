a6100c90e1736a4ae8060e6736b54a92de504c2d
Winston Tsui <ny1winston@gmail.com>
2024-01-30 17:37:40 -0500

    testing

---
+++ exercises/Jan30.java
@@ -0,0 +1,23 @@
+package exercises;
+
+public class Jan30 {
+    /*
+    return an int[] such that for each valid i, result[i] = data[0] + data[1] ... + data[i]
+    For example, if data = {1, 23, 4, 5} result = {1, 24, 28, 33}
+     */
+    public int[] cumulativeSum(int[] data) {
+        return null;
+    }
+    /*
+    return an int[] such that for each valid i, result[i] = data[i] + data[i+1] + data[i+2]
+    For example, if data = {} result = {}
+    For example, if data = {1} result = {1}
+    For example, if data = {1, 23} result = {24}
+    For example, if data = {1, 23, 4} result = {28}
+    For example, if data = {1, 23, 4, 5} result = {28, 32}
+    For example, if data = {1, 23, 4, 5, 10} result = {28, 32, 19}
+     */
+    public int[] threeSum(int[] data) {
+        return null;
+    }
+}
