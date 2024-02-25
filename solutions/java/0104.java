aa8df1a7a62688b9e0f63890fc92059f551a56d5
Winston Tsui <ny1winston@gmail.com>
2024-02-25 14:44:44 -0500

    Added BinarySearchTree notes, made images smaller

--- solutions/java/0104.java
+++ solutions/java/0104.java
@@ -1,6 +1,6 @@
 /*
  * Leetcode 104: Maximum Depth of Binary Tree. Java. Easy.
- * Recursive problem. O(n) runtime and O(log(n)) space complexity.
+ * Recursive problem. O(n) runtime and O(n) space complexity.
  * If at any point the node I'm at is null, the depth is 0.
  * Otherwise, the max depth is one more than the max depth of the left and right children.
  * 2/24/2024 Winston Tsui
