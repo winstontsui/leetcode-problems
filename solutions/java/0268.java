1697c39ece66f21669e5662f5bec91c2aa22c010
Winston Tsui <ny1winston@gmail.com>
2024-03-16 17:13:37 -0400

    Solved Leetcode 268

---
+++ solutions/java/0268.java
@@ -0,0 +1,23 @@
+/*
+ * Leetcode 268: Missing Number. Java. Easy.
+ * O(n) runtime and O(1) space complexity.
+ * The missing number is the expected total sum minus the calculated sum.
+ * 3/16/2024 Winston Tsui
+*/
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int sum = 0;
+
+        // int expectedSum = (len * (len + 1) /2); // Using the sum identity
+        int expectedSum = 0;
+        while (len > -1)
+            expectedSum += len--;
+
+        for (int num : nums)
+            sum += num;
+
+        return expectedSum - sum;
+    }
+}
