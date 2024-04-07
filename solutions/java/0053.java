3c402e62fa9a20128668c2fc26a34ebec9ecc882
Winston Tsui <ny1winston@gmail.com>
2024-04-07 14:36:46 -0400

    Solved Leetcode 53

---
+++ solutions/java/0053.java
@@ -0,0 +1,23 @@
+/*
+ * Leetcode 53: Maximum Subarray. Java. Medium.
+ * O(n) runtime and O(1) space complexity.
+ * Use a variable currSum to track sum up to a certain point. At any given point,
+ * if the sum is less than 0, it cannot be used in later subarrays to achieve max sum, so disregard it.
+ * 4/7/2024 Winston Tsui
+*/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int num : nums) {
+            currSum += num;
+            maxSum = Math.max(maxSum, currSum);
+
+            // If the sum up to this point is negative, remove it as it won't help us later.
+            if (currSum < 0)
+                currSum = 0;
+        }
+        return maxSum;
+    }
+}
