d7d58c291dbca722f3f9f301af6da36c528555b7
Winston Tsui <ny1winston@gmail.com>
2024-04-17 12:03:31 -0400

    Leetcode 48 Transpose and Reflect

--- solutions/java/0048.java
+++ solutions/java/0048.java
@@ -1,9 +1,9 @@
 /*
  * Leetcode 48: Rotate Image. Java. Medium.
  * O(n*m) runtime and O(1) space complexity.
- * Convoluted, tricky problem. Reverse in a layered manner starting from outer layer.
- * Swap elements in a counterclockwise manner 4 at a time, 
- * using an inner loop to increment in each layer. 
+ * Convoluted, tricky problem. Approach 1: Reverse in a layered manner starting from outer layer. 
+ * Swap elements in a counterclockwise manner 4 at a time, using an inner loop to increment in each layer.
+ * Approach 2: Transpose across diagonal and reflect (reverse) elements in every row of matrix.
  * 4/16/2024 Winston Tsui
 */
 
@@ -27,5 +27,24 @@ class Solution {
             left++;
             right--;
         }
+
+        // // Approach 2: Transpose across diagonal and reflect (reverse) elements in every row of matrix.
+        // // Transpose elements along the top-left and bottom-right diagonal.
+        // for (int r = 0; r < matrix.length; r++) {
+        //     for (int c = r; c < matrix[0].length; c++) {
+        //         int temp = matrix[r][c];
+        //         matrix[r][c] = matrix[c][r];
+        //         matrix[c][r] = temp;
+        //     }
+        // }
+
+        // // Reverse contents of every row in matrix from left to right.
+        // for (int r = 0; r < matrix.length; r++) {
+        //     for (int c = 0; c < matrix[0].length / 2; c++) {
+        //         int temp = matrix[r][c];
+        //         matrix[r][c] = matrix[r][matrix[0].length - 1 - c];
+        //         matrix[r][matrix[0].length - 1 - c] = temp;
+        //     }
+        // }
     }
 }
