b3783ac87d32f7e482e29e5e7643f8c76d15505e
Winston Tsui <ny1winston@gmail.com>
2024-05-06 13:27:25 -0400

    Leetcode 105 HashMap O(n) solution

--- solutions/java/0105.java
+++ solutions/java/0105.java
@@ -1,31 +1,51 @@
 /*
  * Leetcode 105: Construct Binary Tree from Preorder and Inorder Traversal. Java. Medium.
- * O(n^2) runtime and O(n) space complexity.
+ * O(n) runtime and O(n) space complexity.
  * Recursively take the first element of preorder (which is always the root), search it up in inorder,
  * and set the node's left and right subtrees to appropriate values in the preorder and inorder arrays
- * based on the size of the subtrees.
- * 5/1/2024 Winston Tsui
+ * based on the size of the subtrees. Use a HashMap to store values to indices.
+ * 5/6/2024 Winston Tsui
 */
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0)
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+        return buildTree(preorder, map, 0, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildTree(int[] preorder, HashMap<Integer, Integer> map, int preorderIndex, int left, int right) {
+        if (right < left)
             return null;
-        TreeNode node = new TreeNode(preorder[0]);
-        int index = 0;
-        while (inorder[index] != preorder[0])
-            index++;
+        int index = map.get(preorder[preorderIndex]);
+        TreeNode node = new TreeNode(preorder[preorderIndex]);
 
-        // Recursive calls depend on the length of left and right subtrees, so modify
-        // preorder and postorder accordingly.
-        node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
-        node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length),
-                Arrays.copyOfRange(inorder, index + 1, inorder.length));
-
+        node.left = buildTree(preorder, map, preorderIndex + 1, left, index - 1);
+        node.right = buildTree(preorder, map, preorderIndex + (index - left) + 1, index + 1, right);
         return node;
     }
+
+    // // Inefficient O(n^2) time complexity solution.
+    // public TreeNode buildTree(int[] preorder, int[] inorder) {
+    //     if (preorder.length == 0)
+    //         return null;
+    //     TreeNode node = new TreeNode(preorder[0]);
+    //     int index = 0;
+    //     while (inorder[index] != preorder[0])
+    //         index++;
+
+    //     // Recursive calls depend on the length of left and right subtrees, so modify
+    //     // preorder and postorder accordingly.
+    //     node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
+    //     node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length),
+    //             Arrays.copyOfRange(inorder, index + 1, inorder.length));
+
+    //     return node;
+    // }
 }
 
 class TreeNode {
