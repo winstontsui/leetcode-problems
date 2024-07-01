3c650f6888f50cb598cd518277db1aeb28ae90d2
winstontsui <ny1winston@gmail.com>
2024-07-01 18:45:40 -0400

    Solved Leetcode 24

---
+++ solutions/java/0024.java
@@ -0,0 +1,48 @@
+/*
+ * Leetcode 24: Swap Nodes in Pairs. Java. Medium.
+ * O(1) runtime and O(capacity) space complexity.
+ * Starting from a previous node, swap nodes in the linked list in pairs by going in steps of 2.
+ * 7/1/2024 Winston Tsui
+*/
+
+class Solution {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+
+        while (curr.next != null && curr.next.next != null) {
+            ListNode first = curr.next;
+            ListNode second = curr.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            curr.next = second;
+
+            curr = first;
+        }
+        return dummy.next;
+    }
+}
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
