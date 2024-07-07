6f2e49b44071d355b75f8ff6e218f6670c907fe8
winstontsui <ny1winston@gmail.com>
2024-07-07 18:40:19 -0400

    Solved Leetcode 904

---
+++ solutions/java/0904.java
@@ -0,0 +1,34 @@
+/*
+ * Leetcode 904: Fruit Into Baskets. Java. Medium.
+ * O(n) runtime and O(1) space complexity.
+ * Sliding window problem. Adjust the window's boundaries when traversing to include a maximum of 2 elements using a HashMap.
+ * Track the window's length accordingly, returning the maximum possible length.
+ * 7/7/2024 Winston Tsui
+ */
+
+import java.util.HashMap;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> fruitCount = new HashMap<>();
+        int maxFruits = 0;
+        int l = 0;
+        int curr = 0; // Current window length
+        
+        for (int r = 0; r < fruits.length; r++) {
+            fruitCount.put(fruits[r], fruitCount.getOrDefault(fruits[r], 0) + 1);
+            curr++;
+            // Shrink left part of window until it's valid.
+            while (fruitCount.size() > 2) {
+                fruitCount.put(fruits[l], fruitCount.get(fruits[l]) - 1);
+                if (fruitCount.get(fruits[l]) == 0)
+                    fruitCount.remove(fruits[l]);
+                l++;
+                curr--;
+            }
+            maxFruits = Math.max(maxFruits, curr);
+
+        }
+        return maxFruits;
+    }
+}
