579f601ecf5d1f78aefd0dc08b7cb81fc0d47d4a
winstontsui <ny1winston@gmail.com>
2024-07-18 18:04:55 -0400

    Solved Leetcode 2971

---
+++ solutions/java/2971.java
@@ -0,0 +1,24 @@
+/*
+ * Leetcode 2971: Find Polygon With the Largest Perimeter. Java. Medium.
+ * O(nlogn) runtime and O(nlogn) space complexity.
+ * Sort nums. Working backwards, determine if nums[0] to nums[i] is a valid polygon, returning the perimeter if it is.
+ * 7/18/2024 Winston Tsui
+*/
+
+import java.util.Arrays;
+
+class Solution {
+    public long largestPerimeter(int[] nums) {
+        long sum = 0;
+        Arrays.sort(nums);
+        for (int num : nums)
+            sum += num;
+
+        for (int i = nums.length - 1; i > 1; i--) {
+            sum -= nums[i];
+            if (sum > nums[i])
+                return sum + nums[i];
+        }
+        return -1;
+    }
+}
