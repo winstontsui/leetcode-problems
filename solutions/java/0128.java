c7f53b16f40cc0d4de720fa9df747ea6e83a0d9c
winstontsui <ny1winston@gmail.com>
2024-10-27 15:44:51 -0400

    Solved Leetcode 128

--- solutions/java/0128.java
+++ solutions/java/0128.java
@@ -33,3 +33,23 @@ class Solution {
         return ans;
     }
 }
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         int longestLen = 0;
+//         for (int i = 0; i < nums.length; i++)
+//             set.add(nums[i]);
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (!set.contains(nums[i] - 1)) {
+//                 int currentLen = 1;
+//                 for (int j = 1; set.contains(nums[i] + j); j++)
+//                     currentLen++;
+//                 longestLen = Math.max(longestLen, currentLen);
+//             }
+
+//         }
+//         return longestLen;
+//     }
+// }
