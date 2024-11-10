4b32081105fe94da2978f62bd586a90dcd42dde1
winstontsui <ny1winston@gmail.com>
2024-11-10 00:41:04 -0500

    Updated Leetcode 153

--- solutions/java/0153.java
+++ solutions/java/0153.java
@@ -30,3 +30,21 @@ class Solution {
         return min;
     }
 }
+
+// class Solution {
+//     public int findMin(int[] nums) {
+//         int l = 0;
+//         int r = nums.length - 1;
+//         while (l < r) {
+//             int mid = (l + r) / 2;
+//             if (mid > 0 && nums[mid] < nums[mid - 1])
+//                 return nums[mid];
+//             else if (nums[mid] > nums[r])
+//                 l = mid + 1;
+//             else
+//                 r = mid - 1;
+
+//         }
+//         return nums[l];
+//     }
+// }
