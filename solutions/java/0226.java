c9b7fdd7f1d22a77a1938650bc91252e78164f6d
winstontsui <ny1winston@gmail.com>
2024-11-12 21:01:14 -0500

    Updated Leetcode 226

--- solutions/java/0226.java
+++ solutions/java/0226.java
@@ -9,12 +9,13 @@ class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null)
             return null;
-
         root.left = invertTree(root.left);
         root.right = invertTree(root.right);
+
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
+
         return root;
     }
 }
