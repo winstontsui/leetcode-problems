6c7f9f25edf0ed521bb382ea83426fc566333079
winstontsuicornell <ny1winston@gmail.com>
2025-06-26 23:39:22 -0700

    Solved Leetcode 35

---
+++ solutions/python/0035.py
@@ -0,0 +1,28 @@
+"""
+ * Leetcode 35: Search Insert Position. Python. Easy.
+ * O(log N) runtime, O(1) space complexity
+ * Binary search to find insertion position in a sorted array.
+ *
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ *
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ *
+ * 6/26/2025 Winston Tsui
+"""
+
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target == nums[m]:
+                return m
+            if target < nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+        return l
